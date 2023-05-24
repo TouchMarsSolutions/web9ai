949f887e7da2c393a26cfc5119f2a51653fe8d1b
John <jhcao23@gmail.com>
2023-05-23 23:28:51 -0700

    update home page content cards

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -11,35 +11,33 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
+    title: 'Web3',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        VR, AR and Decentralization is the passcode to Web3
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Energy',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Energe free is the key milestone of technology
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'AI',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Artificial Intelligence is the wind to blow you into immersive future
       </>
     ),
   },
+  
 ];
 
 function Feature({title, Svg, description}: FeatureItem) {
