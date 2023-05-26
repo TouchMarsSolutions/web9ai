32c5e1d232ca6dc7131d5e68a47fa42b6d308402
John <jhcao23@gmail.com>
2023-05-25 17:28:35 -0700

    add mermaid and sidebar hideable

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,9 +62,18 @@ const config = {
     ],
   ],
 
+  markdown: {
+    mermaid: true
+  },
+
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      docs: {
+        sidebar: {
+          hideable: true,
+        },
+      },
       // Replace with your project's social card
       image: "img/docusaurus-social-card.jpg",
       navbar: {
